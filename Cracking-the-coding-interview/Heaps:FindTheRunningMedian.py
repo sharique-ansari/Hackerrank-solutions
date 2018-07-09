7d4311c05ca0a2eb5e10733dd229b477bcdd76ca
sharique <sharique098@gmail.com>
2018-07-10 01:37:30 +0530

    solved hard question

--- Cracking-the-coding-interview/Heaps:FindTheRunningMedian.py
+++ Cracking-the-coding-interview/Heaps:FindTheRunningMedian.py
@@ -5,14 +5,37 @@ import os
 import random
 import re
 import sys
-
-
+import heapq
 
 if __name__ == '__main__':
     n = int(input())
 
-    a = []
+    half1 = []
+    half2 = []
+    max = 10000000000000
 
     for _ in range(n):
         a_item = int(input())
-        a.append(a_item)
+        len1 = len(half1)
+        len2 = len(half2)
+        if len1 == len2:
+            if a_item < max:
+                heapq.heappush(half1, -a_item)
+                print(float(-half1[0]))
+            else:
+                heapq.heappush(half2, a_item)
+                print(float(half2[0]))
+        elif len1 > len2:
+            if a_item < max:
+                heapq.heappush(half2, -heapq.heappushpop(half1, -a_item))
+
+            else:
+                heapq.heappush(half2, a_item)
+            print((half2[0] - half1[0]) / 2)
+        else:
+            if a_item > max:
+                heapq.heappush(half1, -heapq.heappushpop(half2, a_item))
+            else:
+                heapq.heappush(half1, -a_item)
+            print((half2[0] - half1[0]) / 2)
+        max = -half1[0]
